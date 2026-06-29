237bffff1396e022278d01db7c1b0dfaca761ed0
JUYEON <37200286+white-onne@users.noreply.github.com>
2026-06-29 18:53:31 +0900

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        q=deque()
+        max_num=0
+        visit = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if not visit[i][j] and grid[i][j]==1:
+                    visit[i][j]=True
+                    q.append([i, j])
+                    nums=1
+                    while q:
+                        c_x, c_y=q.pop()
+                        for m_x, m_y in [[0, 1], [1, 0], [-1, 0], [0, -1]]:
+                            n_x, n_y=c_x+m_x, c_y+m_y
+                            if 0<=n_x<len(grid) and 0<=n_y<len(grid[0]) and grid[n_x][n_y]==1 and not visit[n_x][n_y]:
+                                q.append([n_x, n_y])
+                                nums+=1
+                                visit[n_x][n_y]=True
+                    max_num=max(max_num, nums)
+        return max_num
